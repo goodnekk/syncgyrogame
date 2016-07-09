6e7b5d214aac57bb9a79d50100f2d3d16f251424
Marcel Goethals <marcel.goethals@gmail.com>
2016-07-09 17:09:18 +0200

    gameplay

--- index.js
+++ index.js
@@ -5,47 +5,127 @@ var io      = require('socket.io').listen(server);
 
 app.use(express.static('client'));
 
-var count = 0;
-var players = {
 
-};
+//0: waiting
+//1: countdown
+//2: round
+//3: reset time
+
+var timer = 0;
+var gamestate = 0;
+var waitingroom = {};
+
+var count = 0;
+var players = {};
+
+var leader;
 
 io.on('connection', function (socket) {
-    players[count] = {};
+    waitingroom[count] = {
+        ready: false,
+        socket: socket,
+        pos: {}
+    };
+
     var id = count;
     count++;
 
     socket.on('gyro', function (data) {
-        //console.log(data);
-        players[id] = data;
+        if(gamestate === 1){
+            players[id].pos = data;
+        }
+    });
+
+    socket.on('ready', function (data) {
+        if(gamestate === 0){
+            waitingroom[id].ready = data;
+        }
     });
 
     socket.on('disconnect', function () {
-        delete players[id];
+        delete waitingroom[id];
     });
 });
 
 setInterval(function(){
-    var synced = false;
+    //waiting
+    if(gamestate === 0){
+        var playercount = 0;
+        var ready = true;
+        for(var i in waitingroom){
+            playercount++;
+            if(!waitingroom[i].ready){
+                ready = false;
+            }
+        }
+        if(ready && (playercount > 1)) {
+            gamestate = 1;
+            timer = 0;
+            players = waitingroom;
 
-    var leader;
-    for(var i in players){
-        if(!leader){
-            leader = players[i];
-        } else { //compare
-            if(
-                players[i].x > leader.x-5 &&
-                players[i].y > leader.y-5 &&
-                players[i].y > leader.y-5 &&
-                players[i].x < leader.x+5 &&
-                players[i].y < leader.y+5 &&
-                players[i].y < leader.y+5
-            ) {
-                synced = true;
+            io.emit('go');
+            console.log("go");
+
+        }
+    }
+
+    //countdown
+    if(gamestate === 1){
+        console.log("countdown");
+        timer ++;
+        if(timer === 27 ){
+            gamestate = 2;
+            timer = 0;
+        }
+    }
+
+    //pick random leader
+    if(gamestate === 2){
+        console.log("leader");
+        //select random leader
+        gamestate = 3;
+        var keys = [];
+        for (var prop in players) {
+            if (players.hasOwnProperty(prop)) {
+                keys.push(prop);
+            }
+        }
+
+        leader = players[keys[Math.floor(Math.random()*keys.length)]];
+        leader.socket.emit("leader");
+    }
+
+    //
+    if(gamestate === 3){
+        console.log("playing");
+        timer ++;
+        if(timer === 10){
+            timer = 0;
+            gamestate = 4;
+            for(var j in players){
+                if(
+                    players[j].pos.x > leader.pos.x-5 &&
+                    players[j].pos.y > leader.pos.y-5 &&
+                    players[j].pos.y > leader.pos.y-5 &&
+                    players[j].pos.x < leader.pos.x+5 &&
+                    players[j].pos.y < leader.pos.y+5 &&
+                    players[j].pos.y < leader.pos.y+5
+                ) {
+                    players[j].socket.emit("great");
+                } else {
+                    players[j].socket.emit("fail");
+                }
             }
         }
     }
-    console.log(players);
-    console.log(synced);
-    io.emit('update', synced);
+
+    if(gamestate === 4){
+        console.log("results");
+        timer ++;
+        if(timer === 6){
+            timer = 0;
+            gamestate = 2;
+        }
+    }
+
 }, 500);
